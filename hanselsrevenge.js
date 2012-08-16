b0746191b6d1f124b5eaebcbf2b5dca2f1776986
Martin Murphy <martin.murphy@whiteboard-it.com>
2012-08-16 12:25:26 -0500

    small optimizations

--- hanselsrevenge.js
+++ hanselsrevenge.js
@@ -10,8 +10,9 @@
     var breadCrumbContainer = $(this);
     var historyStack = [];
     var historyHash = {};
+    var key = "hanselsrevenge::Stack";
     //options contain the cookie expiration if given etc.
-    var cookieStack = $.cookie("hanselsrevenge::Stack");
+    var cookieStack = $.cookie(key);
     if (cookieStack) {
       historyStack = JSON.parse(cookieStack);
       for (var i = 0; i < historyStack.length; i++) {
@@ -23,17 +24,15 @@
       historyStack.push({ text: document.title, link: path });
     } else {
       var y = historyStack.length - 1;
-      for (; y > 0 && historyStack[y].link != path; y--) {
-        console.log(y);
-      }
+      for (; y > 0 && historyStack[y].link != path; y--) {}
       historyStack = historyStack.slice(0, y + 1);
     }
-    $.cookie("hanselsrevenge::Stack", JSON.stringify(historyStack));
+    $.cookie(key, JSON.stringify(historyStack));
     var maxLinks = 3;
     breadCrumbContainer.html("");
-    historyStack = maxLinks > historyStack.length ? historyStack :  historyStack.slice(historyStack.length - maxLinks);
-    for (var i = historyStack.length-1; i >=0; i--) {
-      var item = historyStack.shift(); 
+    historyStack = maxLinks > historyStack.length ? historyStack : historyStack.slice(historyStack.length - maxLinks);
+    for (var i = historyStack.length - 1; i >= 0; i--) {
+      var item = historyStack.shift();
       (i == 0) ? breadCrumbContainer.append("<li>" + item.text + "</li>") : breadCrumbContainer.append("<li><a href='" + item.link + "'>" + item.text + "</a></li>");
     }
   };
